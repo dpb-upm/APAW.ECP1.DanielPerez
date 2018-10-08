60275423118e4fcf4d5d185dfc5c809b8d1372ee
Daniel Pérez <>
2018-10-08 20:29:59 +0200

    - Creada factoria de servidor

---
+++ src/main/java/es/upm/miw/apaw/ecp1/junit/FactoryServidor.java
@@ -0,0 +1,31 @@
+package es.upm.miw.apaw.ecp1.junit;
+
+public class FactoryServidor {
+
+    private static FactoryServidor factoryServidor;
+    private Servidor servidor;
+
+    public static FactoryServidor getFactory(){
+        if(factoryServidor == null){
+            factoryServidor = new FactoryServidor();
+        }
+        return factoryServidor;
+    }
+
+    public void borrarArchivos() {
+    }
+
+    public Servidor getServidor() {
+        if(this.servidor == null){
+            this.servidor = new Servidor();
+        }
+        return this.servidor;
+    }
+
+    public void setArchivo(Archivo archivo){
+        if(this.servidor == null){
+            this.servidor = new Servidor();
+        }
+        this.servidor.setArchivo(archivo);
+    }
+}
